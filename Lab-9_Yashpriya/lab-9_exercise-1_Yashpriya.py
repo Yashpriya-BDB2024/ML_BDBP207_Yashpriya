0bb531e059a163da7947df9949137df01ebde2ea
Yash Priya Baid <yashpriyabaid@gmail.com>
2025-03-11 07:15:19 +0530

    Comments added

--- Lab-9_Yashpriya/lab-9_exercise-1_Yashpriya.py
+++ Lab-9_Yashpriya/lab-9_exercise-1_Yashpriya.py
@@ -1,12 +1,12 @@
-# Write a program to partition a dataset (simulated data for regression) into two parts, based on a feature (BP) and for a threshold, t = 80.
-# Generate additional two partitioned datasets based on different threshold values of t = [78, 82].
+### Write a program to partition a dataset (simulated data for regression) into two parts, based on a feature (BP) and for a threshold, t = 80.
+### Generate additional two partitioned datasets based on different threshold values of t = [78, 82].
 
 import pandas as pd
 
 def partition_space(data, thresholds):
-    for t in thresholds:
-        lower_partition = data[data["BP"] <= t]
-        upper_partition = data[data["BP"] > t]
+    for t in thresholds:     # Iterate through each threshold value
+        lower_partition = data[data["BP"] <= t]      # Create lower partition where BP is less than or equal to the threshold
+        upper_partition = data[data["BP"] > t]       # Create upper partition where BP is greater than the threshold
         print(f"\nFor threshold t = {t}:")
         print(f"Lower Partition (BP ≤ {t}):")
         print(lower_partition)
@@ -14,9 +14,9 @@ def partition_space(data, thresholds):
         print(upper_partition)
 
 def main():
-    data =  pd.read_csv("simulated_data_multiple_linear_regression_for_ML.csv")
-    thresholds = [80, 78, 82]
-    partition_space(data, thresholds)
+    data =  pd.read_csv("simulated_data_multiple_linear_regression_for_ML.csv")     # Read the dataset from a CSV file
+    thresholds = [80, 78, 82]        # Define different threshold values for partitioning
+    partition_space(data, thresholds)      # Call the partition function to divide data based on threshold values
 
 if __name__ == "__main__":
     main()
